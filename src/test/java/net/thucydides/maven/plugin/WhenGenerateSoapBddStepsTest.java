e18bfe76dfd3680bcdabd2a0e25825cc73fffe09
dmytro.gryshchenko <dmytro.gryshchenko@engagepoint.com>
2015-03-06 17:27:05 +0200

    Add test generateBddStepsFormSoapCheckClassFiles

--- src/test/java/net/thucydides/maven/plugin/WhenGenerateSoapBddStepsTest.java
+++ src/test/java/net/thucydides/maven/plugin/WhenGenerateSoapBddStepsTest.java
@@ -11,13 +11,13 @@ import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class WhenGenerateSoapBddStepsTest {
     private String expectedPathToFile = "./src/test/java/net/thucydides/maven/plugin/test/example/HelloWorldImplServiceSteps.java";
@@ -40,7 +40,7 @@ public class WhenGenerateSoapBddStepsTest {
     }
 
     @Test
-    public void generateBddStepsFormSoap() throws IOException {
+    public void generateBddStepsFormSoapCheckNumberLinesInFile() throws IOException {
         try {
             plugin.execute();
         } catch (MojoExecutionException e) {
@@ -49,24 +49,19 @@ public class WhenGenerateSoapBddStepsTest {
             e.printStackTrace();
         }
         File destinationDirectory = new File(outputDirectory, plugin.soapServicePackages[0].replaceAll("\\.", "/"));
+
         File actualFile = new File(destinationDirectory, "HelloWorldImplServiceSteps.java");
         File expectedFile = new File(expectedPathToFile);
         List<String> actualFileContent = FileUtils.readLines(actualFile);
         List<String> expectedFileContent = FileUtils.readLines(expectedFile);
         assertEquals("Number of lines in files is different", expectedFileContent.size(), actualFileContent.size());
-        SortedSet<String> actualFileCodeBlocks = splitFileByEmptyLines(actualFileContent);
-        SortedSet<String> expectedFileCodeBlocks = splitFileByEmptyLines(expectedFileContent);
-        assertTrue("Files are not equivalent", actualFileCodeBlocks.equals(expectedFileCodeBlocks));
-//        assertTrue("Files are not equivalent", CollectionUtils.isEqualCollection(expectedFileCodeBlocks, actualFileCodeBlocks));
     }
 
-    private SortedSet<String> splitFileByEmptyLines(List<String> fileLines) {
+    private List<String> splitFileByEmptyLines(List<String> fileLines) {
         List<String> result = new ArrayList<String>();
-        SortedSet<String> set = new TreeSet<String>();
         StringBuilder stringBuilder = new StringBuilder();
         for (String line : fileLines) {
             if (line.isEmpty()) {
-                set.add(stringBuilder.toString());
                 result.add(stringBuilder.toString());
                 //clean string builder
                 stringBuilder.setLength(0);
@@ -76,7 +71,27 @@ public class WhenGenerateSoapBddStepsTest {
         }
         //add last line
         result.add(stringBuilder.toString());
-        set.add(stringBuilder.toString());
-        return set;
+        return result;
+    }
+
+    @Test
+    public void generateBddStepsFormSoapCheckClassFiles() throws Exception {
+        File expectedfile = new File(expectedPathToFile);
+        File actualFile = new File(outputDirectory.toURI());
+        try {
+            URL expUrl = expectedfile.toURL();
+            URL ActualUrl = actualFile.toURL();
+            URL[] expUrls = new URL[]{expUrl};
+            URL[] actUrls = new URL[]{ActualUrl};
+            ClassLoader expCL = new URLClassLoader(expUrls);
+            ClassLoader actualCL = new URLClassLoader(actUrls);
+            Class expResult = expCL.loadClass("net.thucydides.maven.plugin.test.example.HelloWorldImplServiceSteps");
+            Class actualResult = actualCL.loadClass("net.thucydides.maven.plugin.test.example.HelloWorldImplServiceSteps");
+            assertEquals("Files are not equivalent", expResult, actualResult);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
